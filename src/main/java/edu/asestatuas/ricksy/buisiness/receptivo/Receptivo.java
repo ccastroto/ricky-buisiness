34a87332b28358a032ca646908317c66bce6c489
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-07 10:59:03 +0200

    refactor(receptivo): definir el registra

--- src/main/java/edu/asestatuas/ricksy/buisiness/receptivo/Receptivo.java
+++ src/main/java/edu/asestatuas/ricksy/buisiness/receptivo/Receptivo.java
@@ -1,12 +1,19 @@
 package edu.asestatuas.ricksy.buisiness.receptivo;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Receptivo {
 
     private static Receptivo instance = null;
+    private Set<GuestDispatcher> observers = new LinkedHashSet<>();
 
     private Receptivo() {}
 
     public static Receptivo getReceptivo() {
         return instance == null ? new Receptivo() : instance;
+    }
+    public void registra(GuestDispatcher observer) {
+        this.observers.add(observer);
     }
 }
