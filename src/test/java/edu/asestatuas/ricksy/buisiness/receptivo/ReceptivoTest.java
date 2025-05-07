474deb7348bdff240715a547e61cc48362d8135d
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-07 10:44:38 +0200

    feat(receptivo): Implementar los casos test y comentar la mayoría

---
+++ src/test/java/edu/asestatuas/ricksy/buisiness/receptivo/ReceptivoTest.java
@@ -0,0 +1,65 @@
+package edu.asestatuas.ricksy.buisiness.receptivo;
+
+import org.junit.Test;
+
+import edu.asestatuas.ricksy.buisiness.dispatchers.CrystalExpender;
+import edu.asestatuas.ricksy.buisiness.dispatchers.UfosParkTest;
+import edu.asestatuas.ricksy.buisiness.payment.CreditCard;
+import edu.asestatuas.ricksy.buisiness.payment.PaymentMethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class ReceptivoTest {
+
+    private Receptivo receptivo = null;
+    private UfosParkTest parkTest = null;
+    private CrystalExpender packExpender = null;
+
+    @Before
+    public void setupTest() {
+
+        parkTest = new UfosParkTest();
+        parkTest.setupUfosPark();
+
+        packExpender = new CrystalExpender(100, 50);
+
+        receptivo = Receptivo.getReceptivo();
+        receptivo.registra(parkTest.ufos);
+        receptivo.registra(packExpender);
+    }
+
+    @Test
+    public void SingletonReceptivo() {
+        receptivo = Receptivo.getReceptivo();
+        assertNotNull(receptivo);
+        Receptivo otroReceptivo = Receptivo.getReceptivo();
+        assertEquals(receptivo, otroReceptivo);
+        assertSame(receptivo, otroReceptivo);
+    }
+
+/*    @Test
+    public void dispatchTest() {
+
+        PaymentMethod card = new CreditCard("Abradolf Lincler", "4916119711304546");
+        receptivo.dispatch(card);
+
+        assertEquals(2450, card.credit(), 0);
+        assertTrue(parkTest.ufos.containsCard(card.number()));
+        assertEquals(99, packExpender.stock());
+    }
+
+/*    @Test
+    public void dispatchNoCreditTest() {
+
+        PaymentMethod card = new CreditCard("Abradolf Lincler", "4916119711304546");
+        assertTrue(card.pay(2990));
+        assertEquals(10, card.credit(), 0);
+        receptivo.dispatch(card);
+        assertEquals(10, card.credit(), 0);
+        assertFalse(parkTest.ufos.containsCard(card.number()));
+        assertEquals(100, packExpender.stock());
+    }*/
+
+}
