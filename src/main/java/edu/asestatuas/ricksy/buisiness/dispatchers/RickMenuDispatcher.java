6c6878043c810f9536446efa7eb46620d8d723bf
a24carlosca <castroaldrovercarlossangrinha@gmail.com>
2025-05-07 11:18:31 +0200

    feat(rickmenudispatcher): Definir el RickMenuDispatcher y definir el dispatch

---
+++ src/main/java/edu/asestatuas/ricksy/buisiness/dispatchers/RickMenuDispatcher.java
@@ -0,0 +1,27 @@
+package edu.asestatuas.ricksy.buisiness.dispatchers;
+
+import edu.asestatuas.ricksy.buisiness.payment.PaymentMethod;
+import edu.asestatuas.ricksy.buisiness.receptivo.GuestDispatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RickMenuDispatcher implements GuestDispatcher {
+
+    private int stock = 100;
+    private double menuCost = 10d;
+
+    private List<String> orders = new ArrayList<String>();
+
+    public RickMenuDispatcher() {}
+
+    @Override
+    public void dispatch(PaymentMethod card){
+        if (this.stock > 0 && card.pay(menuCost)){
+            this.stock -= 1;
+            this.orders.add(card.cardOwner());
+        }
+    }
+
+
+}
